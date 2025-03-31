582618725273299f54979a762c472494eb70d0fc
maiko-001 <mai.fitness1015@gmail.com>
2025-03-31 21:55:40 +0900

    Add files via upload

---
+++ kadai-011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+
+<body>
+      <?php
+        $veg_info = ['名前'=>'玉ねぎ','値段'=>'200','産地'=>'北海道'];
+
+        foreach($veg_info as $key => $value){
+          echo "{$key}:{$value}<br>";
+        }
+      ?>
+</body>
+</html>
